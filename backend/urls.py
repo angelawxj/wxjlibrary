432cff421602cc78e3fc35c19d480ef03d049b9b
wxjprogram <2472569272@qq.com>
2018-05-25 11:35:51 +0800

    提交代码

--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('bookList/', include('bookList.urls')),
+	path('', include('bookList.urls')),
 ]
